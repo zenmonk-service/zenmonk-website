84592256ab4f05ada97ff99da6d3b54594b7ec52
ChiragSethi-1153 <chirag.1153@zenmonk.tech>
2025-04-08 17:09:38 +0530

    Feature: Add CEO section with text and logo images, and integrate into careers page

---
+++ src/modules/careers/sections/words-by-ceo/index.tsx
@@ -0,0 +1,19 @@
+import { Box, Stack } from '@mui/material'
+import Image from 'next/image'
+import { CeoTextImage, ZenmonkLogo } from '../../assets/ceo-section'
+import './styles.scss'
+
+const WordsByCEO = () => {
+  return (
+    <Stack className='ceo-section-wrapper' >
+      <Box className="ceo-section-text-image">
+        <Image alt='' src={CeoTextImage} />
+      </Box>
+      <Box className="ceo-section-logo">
+        <Image alt='' src={ZenmonkLogo} />
+      </Box>
+    </Stack>
+  )
+}
+
+export default WordsByCEO
