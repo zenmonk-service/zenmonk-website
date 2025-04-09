d6b3c7a115c08224cd7d1bc19028ec17461c0aae
Pavitar Singh <pavitar.1127@zenmonk.tech>
2025-04-09 16:14:31 +0530

    Feature: Fix about page conflicts

--- src/modules/about-us/sections/award-proof-section/index.tsx
+++ src/modules/about-us/sections/award-proof-section/index.tsx
@@ -7,7 +7,6 @@ import './styles.scss'
 
 export const AwardProofSection = () => {
   return (
-<<<<<<< Updated upstream
     <Box className="award-proof-section">
       <Title
         text="Prestigious Accolades That We Have Received Over Time"
@@ -30,31 +29,5 @@ export const AwardProofSection = () => {
         })}
       </Box>
     </Box>
-=======
-    <AboutSectionWrapper>
-      <Box className="award-proof-section">
-        <Title
-          text="Awards Proof Your Business With Our IT Services"
-          className="section-title"
-        />
-        <Typography component="p" className="section-description">
-          Lorem Ipsum has been the industry's standard dummy text ever since the
-          1500s. Lorem Ipsum has been the industry's standard{' '}
-        </Typography>
-        <Box className="award-proof-list">
-          {awardProof.map((award, index) => {
-            return (
-              <AwardProofCard
-                description={award.description}
-                image={award.image}
-                title={award.title}
-                key={index}
-              />
-            )
-          })}
-        </Box>
-      </Box>
-    </AboutSectionWrapper>
->>>>>>> Stashed changes
   )
 }
