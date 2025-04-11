d477042c678abff088ef2a5e364f4cabe64c9d28
Chandan <chandan.1139@zenmonk.tech>
2025-04-11 17:52:14 +0530

    feature: fixed ui/ux hero section

--- src/modules/services/modules/ui-ux/hero-section/index.tsx
+++ src/modules/services/modules/ui-ux/hero-section/index.tsx
@@ -13,7 +13,7 @@ import {
   Rope,
   Square,
   Star,
-  Flower
+  Flower,
 } from '../assets'
 import './styles.scss'
 
@@ -43,31 +43,19 @@ const UiUx: React.FC = () => (
           </Grid>
 
           <Grid item xs={12} md={5}>
-            <Image
-              className="hero-image"
-              src={Hero.src}
-              alt="Hero Image"
-              width={500}
-              height={300}
-              style={{
-                width: '100%',
-                height: 'auto',
-                objectFit: 'contain',
-                borderRadius: '8px',
-              }}
-            />
+            <Hero className="hero-image" />
           </Grid>
         </Grid>
-        <Image src={Pen} alt="Pen" className="pen" />
-        <Image src={Bracket} alt="Bracket" className="bracket" />
-        <Image src={Flower} alt="Flower" className="flower" />
-        <Image src={ColorBrush} alt="ColorBrush" className="color-brush" />
-        <Image src={FilledStar} alt="FilledStar" className="filled-star" />
-        <Image src={PaintBrush} alt="PaintBrush" className="paint-brush" />
-        <Image src={Ring} alt="Ring" className="ring" />
-        <Image src={Rope} alt="Rope" className="rope" />
-        <Image src={Star} alt="Star" className="star" />
-        <Image src={Square} alt="Square" className="square" />
+        <Pen className="pen" />
+        <Bracket className="bracket" />
+        <Flower className="flower" />
+        <ColorBrush className="color-brush" />
+        <FilledStar className="filled-star" />
+        <PaintBrush className="paint-brush" />
+        <Ring className="ring" />
+        <Rope className="rope" />
+        <Star className="star" />
+        <Square className="square" />
       </SectionWrapper>
     </Box>
   </>
