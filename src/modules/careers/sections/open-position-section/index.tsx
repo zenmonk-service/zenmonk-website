fc6ccdd22887d79f77a8a5fc49b5287328ed741a
Chandan <chandan.1139@zenmonk.tech>
2025-04-15 18:35:42 +0530

    Feature: Responsive design for 8k

--- src/modules/careers/sections/open-position-section/index.tsx
+++ src/modules/careers/sections/open-position-section/index.tsx
@@ -1,16 +1,14 @@
 import { Box, Container, Typography } from '@mui/material'
-import Title from '@/shared/title'
 import Positions from './positions'
 import './styles.scss'
+import { SectionTitle } from '@/shared/typography'
 
 const OpenPosition = () => {
   return (
     <Box className="open-position-container">
       <Box className="open-position-title-wrapper">
-        <Typography className="open-position-title" component="h1">
-          Explore Open Positions and Join
-        </Typography>
-        <Title className="open-position-sub-title" text="Our Team" />
+        <SectionTitle className="open-position-title" text="Explore Open Positions and Join Our Team" markText=''>
+        </SectionTitle>
       </Box>
       <Typography className="description" component="p">
         Discover a wide range of exciting career opportunities and take the next
--- src/modules/careers/sections/work-culture-section/index.tsx
+++ src/modules/careers/sections/work-culture-section/index.tsx
@@ -5,6 +5,7 @@ import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore'
 import NavigateNextIcon from '@mui/icons-material/NavigateNext'
 import { Box, Container, IconButton, Typography } from '@mui/material'
 import Title from '@/shared/title'
+import { SectionDescription, SectionTitle } from '@/shared/typography'
 import CultureCard from './card/culture-card'
 import './styles.scss'
 import { workCultures } from './work-cultures'
@@ -30,24 +31,29 @@ const WorkCulture = () => {
   )
 
   return (
-    <Container maxWidth="xl">
+    <Container style={{ maxWidth: '100%' }}>
       <Box className="work-culture-section">
         <Box className="title-description-action-wrapper">
           <Box className="title-description-wrapper">
-            <Title
+            <SectionTitle
               align="left"
               text="Zen Focused Work Environment"
               className="work-culture-title"
             />
-            <Typography className="work-culture-description" component="p">
-              At Zenmonk, we cultivate serenity and growth, balancing
+            <SectionDescription
+              className="work-culture-description"
+              text="At Zenmonk, we cultivate serenity and growth, balancing
               professional success with personal well-being. Blending
               mindfulness with innovation, we nurture a supportive community
-              where continuous learning and creative fulfillment prosper.
-            </Typography>
+              where continuous learning and creative fulfillment prosper."
+            ></SectionDescription>
           </Box>
           <Box className="action-button-wrapper">
-            <IconButton className="action-button" onClick={handlePrev} centerRipple >
+            <IconButton
+              className="action-button"
+              onClick={handlePrev}
+              centerRipple
+            >
               <NavigateBeforeIcon fontSize="inherit" />
             </IconButton>
             <IconButton className="action-button" onClick={handleNext}>
