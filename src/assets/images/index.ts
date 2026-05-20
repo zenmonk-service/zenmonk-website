d6c4b5e9fae168f31f4ee75f4e43f7985e078411
Chandan <chandan.1139@zenmonk.tech>
2026-05-20 18:27:13 +0530

    Removed unused imports

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,15 +1,5 @@
-import Laptop2 from './laptop2.png'
-import SphereGif from './sphere.gif'
-import HeroTech from './tech2.png'
-import ZenmonkLogoV2 from './zenmonk-logo-v2.png'
-import ZenmonkLogoV3 from './zenmonk-logo-v3.png'
 import ZenmonkLogo from './zenmonk-logo.png'
 
 export {
-  HeroTech,
   ZenmonkLogo,
-  Laptop2,
-  ZenmonkLogoV2,
-  ZenmonkLogoV3,
-  SphereGif,
 }
